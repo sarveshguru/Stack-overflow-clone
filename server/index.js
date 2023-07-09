77cfb52d6c629d4478103e7faec36f106727a94a
sarveshguru <csarvesh288@gmail.com>
2023-07-10 05:05:34 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -38,6 +38,16 @@ const PORT = process.env.PORT || 5000
 
 const DATABASE_URL = process.env.CONNECTION_URL
 
+// mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
+//     .catch((err) => console.log(err.message))
+
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
-    .catch((err) => console.log(err.message))
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err.message);
+  });
