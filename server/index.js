cc764f2f387b7d1b2be625c370f356cbe00bc1cc
sarveshguru <csarvesh288@gmail.com>
2023-07-10 05:45:55 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -20,34 +20,25 @@ app.use(cors());
 
 
 
-app.get('/', (req, res) => {                              
+app.get('/', (req, res) => {
     res.send("This is a stack overflow clone API")
 })
 
 
-app.use('/user', userRoutes)    
-app.use('/questions', questionsRoutes)    
-app.use('/answer', answerRoutes)    
-app.use('/posts', postsRoutes)    
+app.use('/user', userRoutes)
+app.use('/questions', questionsRoutes)
+app.use('/answer', answerRoutes)
+app.use('/posts', postsRoutes)
 // app.use('/subs', subsRoutes)    
 
 
-const PORT = process.env.PORT || 5000   
+const PORT = PORT || 5000
 
 
 
 const DATABASE_URL = process.env.CONNECTION_URL
 
-// mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
-//     .catch((err) => console.log(err.message))
 
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error(err.message);
-  });
+    .then(() => app.listen(PORT, () => { console.log(`server running on port ${PORT}`) }))
+    .catch((err) => console.log(err.message))
